9dadfff64853f60e1732d658d305919477d6bef6
indianpojken <io.qrdr@gmail.com>
2022-11-02 17:23:31 +0100

    remove files and fix formatting.

--- js/index.js
+++ js/index.js
@@ -1,139 +1,113 @@
-const gameTimerEl = document.querySelector(".game-timer");
-const letterBoardEl = document.querySelector(".main__letter-board");
-const guessedLetterBoardEl = document.querySelector(
-  ".main__guessed-letters-board"
-);
+const elements = {
+  timer: document.querySelector('.game-timer'),
+  letterboard: document.querySelector('.main__letter-board'),
+  guessedLetters: document.querySelector('.main__guessed-letters-board'),
+  gameMessage: document.querySelector('.game-message'),
+  figure: document.querySelector('figure'),
+};
 
 const originalWords = [
-  "fiskmås",
-  "parameter",
-  "osthyvel",
-  "tangentbord",
-  "skärmsläckare",
-  "fotboll",
-  "variabel",
-  "javascript",
-  "programmering",
-  "fiskpinne",
+  'fiskmås', 'parameter', 'osthyvel',
+  'tangentbord', 'skärmsläckare', 'fotboll',
+  'variabel', 'javascript', 'programmering',
+  'fiskpinne',
 ];
 
 let words = [...originalWords];
-let wordArr;
 
-let guessedWord = [];
-let guessedLetters = [];
+const gameState = {};
 
-let failures = 0;
-let timer;
-let gameTimer = 60;
-
-const model = ["scaffold", "head", "body", "arms", "legs"];
+const figure = {
+  parts: ['scaffold', 'head', 'body', 'arms', 'legs'],
+  clear: function () {
+    this.parts.forEach((part) => {
+      elements.figure.classList.remove(part);
+    });
+  },
+};
 
 function getRandomWord() {
-  let randomIndex = Math.floor(Math.random() * words.length);
-  let word = words.splice(randomIndex, 1)[0];
+  const randomIndex = Math.floor(Math.random() * words.length);
+  const [word] = words.splice(randomIndex, 1);
   if (!words.length) words = [...originalWords];
   return word;
 }
 
 function startNewGame() {
-  wordArr = [...getRandomWord()];
-  guessedWord = Array(wordArr.length).fill("");
-  guessedLetters = [];
-  failures = 0;
-  gameTimer = 60;
-  gameTimerEl.innerHTML = `<p>${gameTimer}</p>`;
+  gameState.word = [...getRandomWord()];
+  gameState.correctLetters = Array(gameState.word.length).fill('');
+  gameState.guessedLetters = [];
+  gameState.guesses = 0;
+  gameState.time = 60;
 
-  timer = setInterval(() => {
-    gameTimer -= 1;
-    gameTimerEl.innerHTML = `<p>${gameTimer}</p>`;
-
-    if (gameTimer === 0) {
-      renderWinOrLoss("loss")
-    }
+  gameState.timer = setInterval(() => {
+    gameState.time -= 1;
+    elements.timer.innerHTML = `<p>${gameState.time}</p>`;
+    if (gameState.time === 0) renderMessage('loss');
   }, 1000);
 
-  document.querySelector(".game-message").innerHTML = "";
-  document.querySelector(".game-message").classList.toggle("hidden");
-
-  model.forEach((part) => {
-    document.querySelector("figure").classList.remove(part);
-  });
-
+  elements.timer.innerHTML = `<p>${gameState.time}</p>`;
+  elements.gameMessage.classList.toggle('hidden');
+  figure.clear();
   renderBoards();
-  console.log(wordArr);
 }
-
-startNewGame();
 
 function renderBoards() {
-  letterBoardEl.innerHTML = guessedWord.reduce(
-    (acc, v) =>
-      (acc += !v.length
-        ? `<article class='letter-board__letter-slot'>_</article>`
-        : `<article class='letter-board__letter-slot'>${v}</article>`),
-    ""
-  );
+  elements.letterboard.innerHTML =
+    gameState.correctLetters.reduce((string, letter) =>
+      string += letter !== ''
+        ? `<article class='letter-board__letter-slot'>${letter}</article>`
+        : `<article class='letter-board__letter-slot'>_</article>`, '');
 
-  guessedLetterBoardEl.innerHTML = guessedLetters.reduce(
-    (acc, v) =>
-      guessedWord.includes(v)
-        ? (acc += `<span class='guessed-letter'>${v}</span>`)
-        : (acc += `<span class='guessed-letter guessed-letter--wrong'>${v}</span>`),
-    ""
-  );
+  elements.guessedLetters.innerHTML =
+    gameState.guessedLetters.reduce((string, letter) =>
+      string += gameState.correctLetters.includes(letter)
+        ? `<span class='guessed-letter'>${letter}</span>`
+        : `<span class='guessed-letter guessed-letter--wrong'>${letter}</span>`, '');
 }
 
-function renderWinOrLoss(type) {
-  if (type === "win") {
-    document.querySelector(".game-message").classList.toggle("hidden");
-    document.querySelector(".game-message").innerHTML = `
-      <p>Grattis, du vann!</p>
-      <button onclick="startNewGame()">Spela igen!</button>
-    `;
+function renderMessage(type) {
+  elements.gameMessage.classList.toggle('hidden');
+  const button = `<button onclick='startNewGame()'>Spela igen!</button>`;
 
-    model.forEach((part) => {
-      document.querySelector("figure").classList.remove(part);
-    });
-  } else if (type === "loss") {
-    document.querySelector(".game-message").classList.toggle("hidden");
-    document.querySelector(".game-message").innerHTML = `
-      <p>Tyvärr, du förlorade!</p>
-      <button onclick="startNewGame()">Spela igen!</button>
-    `;
+  if (type === 'win') {
+    elements.gameMessage.innerHTML = `<p>Grattis, du vann!</p>${button}`;
+    figure.clear();
+  } else if (type === 'loss') {
+    gameState.correctLetters = gameState.word;
+    renderBoards();
+    elements.gameMessage.innerHTML = `<p>Tyvärr, du förlorade!</p>${button}`;
   }
 
-  clearInterval(timer);
+  clearInterval(gameState.timer);
 }
 
-document.addEventListener("keydown", (event) => {
+document.addEventListener('keydown', (event) => {
   const isLetter = (letter) => !!letter.match(/^[a-zåäö]$/i);
   const guessedLetter = event.key.toLowerCase();
 
-  if (
-    isLetter(guessedLetter) &&
-    !guessedLetters.includes(guessedLetter) &&
-    failures <= 4 &&
-    gameTimer !== 0
-  ) {
-    if (wordArr.includes(guessedLetter)) {
-      wordArr.forEach((letter, i) => {
-        if (letter === guessedLetter) guessedWord[i] = letter;
+  if (isLetter(guessedLetter)
+    && !gameState.guessedLetters.includes(guessedLetter)
+    && gameState.guesses !== 5
+    && gameState.time !== 0) {
+    if (gameState.word.includes(guessedLetter)) {
+      gameState.word.forEach((letter, index) => {
+        if (letter === guessedLetter) gameState.correctLetters[index] = letter;
       });
     } else {
-      document.querySelector("figure").classList.add(model[failures]);
-      failures += 1;
+      elements.figure.classList.add(figure.parts[gameState.guesses++]);
     }
 
-    guessedLetters.push(guessedLetter);
+    gameState.guessedLetters.push(guessedLetter);
+
+    if (gameState.correctLetters.join('') === gameState.word.join('')) {
+      renderMessage('win');
+    } else if (gameState.guesses === 5) {
+      renderMessage('loss');
+    }
 
     renderBoards();
-
-    if (guessedWord.join("") === wordArr.join("")) {
-      renderWinOrLoss("win");
-    } else if (failures > 4) {
-      renderWinOrLoss("loss");
-    }
   }
 });
 
+startNewGame();
